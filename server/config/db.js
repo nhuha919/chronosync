e6319eb860f6a1048ab8f398f294573761d9f47f
Villie <villie@dyn204207.cc.lehigh.edu>
2025-10-21 21:01:25 -0400

    config db

---
+++ server/config/db.js
@@ -0,0 +1,19 @@
+import dotenv from 'dotenv';
+import pkg from 'pg';
+const { Pool } = pkg;
+
+dotenv.config();
+
+const pool = new Pool({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  ssl: {
+    require: true,
+    rejectUnauthorized: false,  
+  },
+});
+
+export default pool;
